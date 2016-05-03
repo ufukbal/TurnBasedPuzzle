d7dde17c7f5c5c8f5150280a4653be165b573974
ufukbal <balufuk91@gmail.co>
2016-05-03 22:57:39 +0300

    additions on levels, scripts

---
+++ Assets/Scripts/MaskManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaskManager : MonoBehaviour {
+	public float rateOfMovement = 5500f;
+
+	Vector3 leftPosition =  new Vector3 (-812,0,0);
+	Vector3 rightPosition =  new Vector3 (812,0,0);
+
+	public void OpenRightMask(){
+		StartCoroutine (MoveMask (leftPosition));
+	}
+	public void OpenLeftMask(){
+		StartCoroutine (MoveMask (rightPosition));
+	}
+
+	IEnumerator MoveMask(Vector3 newPos){
+		while (true) {
+			if (transform.localPosition.x == newPos.x) {
+				rateOfMovement = 5500;
+				break;
+			}
+			transform.localPosition = Vector3.MoveTowards (transform.localPosition, newPos, Time.deltaTime * rateOfMovement);
+		//	rateOfMovement/=1.05f;
+
+			yield return new WaitForSeconds (0.0001f);
+		}
+
+
+
+
+	}
+
+}
---
+++ Assets/Scripts/SmoothCamera2D.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothCamera2D : MonoBehaviour {
+
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	public Transform target;
+	public Camera cam;
+	public bool isCameraMoving;
+
+	void Start(){
+		
+	}
+	void Update () 
+	{
+		if (target)
+		{
+			Vector3 point = cam.WorldToViewportPoint(target.position);
+			Vector3 delta = target.position - cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+			Vector3 destination = cam.transform.position + delta;
+			cam.transform.position = Vector3.SmoothDamp(cam.transform.position, destination, ref velocity, dampTime);
+		}
+
+	}
+	void OnTriggerEnter(Collider other) {
+		if (target)
+		{
+		//	if(other.tag == "CameraTrack")
+			//	StartCoroutine ("SmoothCamera");
+
+		}
+	}
+
+	IEnumerator SmoothCamera() {
+		isCameraMoving = true;
+		float elapsedTime = 0f;
+		Vector3 inititalUp = transform.up;
+
+		while (elapsedTime < dampTime) {
+			Vector3 point = cam.WorldToViewportPoint(target.position);
+			Vector3 delta = target.position - cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+			Vector3 destination = cam.transform.position + delta;
+			cam.transform.position = Vector3.SmoothDamp(cam.transform.position, destination, ref velocity, elapsedTime/dampTime);
+			elapsedTime += Time.deltaTime;
+			yield return null;
+		}
+
+
+		isCameraMoving = false;
+	}
+
+}
